a4c0309d0ffd723c0511e56cda7f75eb748a36b5
kubah <kubah@git>
2015-06-03 03:11:46 +0200

    Add new functions. Fix few bugs.

--- process.py
+++ process.py
@@ -3,9 +3,11 @@ import os
 import main
 
 
+dataDir = "data"
+
 def test(dirname):
     for file in os.listdir(dirname):
         main.process(dirname + '/' + file)
 
 if __name__=="__main__":
-    test("data")
+    test(dataDir)
